d3dac1e0aba046a1a9c0c304d2872d479888f6da
alihamidi5736 <30084639@lcwc.ac.uk>
2024-09-24 19:38:45 +0100

    Update task.py

--- task.py
+++ task.py
@@ -1,11 +1,30 @@
 def task_1():
     password = "Password123"
 
-    # Enter your code here
+    guess = ""
+    
+    while guess != password:
+     guess = input ("please enter the password: ");
+       
 
 def task_2(): # Times table
 
     # Enter your code here
+    table = int(input("Select time table:" ))
+
+    terms = int(input("how many terms to calculate:"))
+
+    counter = 1
+    
+    while  counter <= terms:
+       print(table*counter)
+       counter += 1; 
 
 def task_3(): # Count mississippis
     
+    import time
+     counter = 0
+     while counter <5:
+        print(f"{counter + 1} mississipi")
+        time.sleep(1)
+        print(task_3)
